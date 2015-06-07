d29f0fd853528ba65021f1d5990c04911b6939d8
Evan Jenkins <evan@activelamp.com>
2015-06-07 16:44:24 -0700

    fixing gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,7 +133,19 @@ module.exports = function (grunt) {
       },
       dist: {
         options: {
-          generatedImagesDir: '<%= yeoman.dist %>/img/generated'
+          generatedImagesDir: '<%= yeoman.dist %>/img/generated',
+          // If you're using global Sass gems, require them here.
+          // require: ['singularity', 'jacket'],
+          sassDir: '<%= yeoman.app %>/_scss',
+          cssDir: '.tmp/css',
+          imagesDir: '<%= yeoman.app %>/images',
+          javascriptsDir: '<%= yeoman.app %>/js',
+          relativeAssets: false,
+          httpImagesPath: '/images',
+          httpGeneratedImagesPath: '/images/generated',
+          outputStyle: 'expanded',
+          raw: 'extensions_dir = "<%= yeoman.app %>/_bower_components"\n',
+          importPath: ['<%= yeoman.app %>/_bower_components/foundation/scss', '<%= yeoman.app %>/_bower_components/fontawesome/scss', '<%= yeoman.app %>/_bower_components/chartist/dist/scss'],
         }
       },
       server: {
@@ -334,7 +346,7 @@ module.exports = function (grunt) {
         'jekyll:server'
       ],
       dist: [
-        'compass:server',
+        'compass:dist',
         'svgstore',
         'copy:dist'
       ]
