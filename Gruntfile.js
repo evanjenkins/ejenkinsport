4df594e7bc1e8095a04350a1e3338d6910776a76
Evan Jenkins <evan@activelamp.com>
2015-06-07 16:49:19 -0700

    fixing issues for build

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,19 +133,7 @@ module.exports = function (grunt) {
       },
       dist: {
         options: {
-          generatedImagesDir: '<%= yeoman.dist %>/img/generated',
-          // If you're using global Sass gems, require them here.
-          // require: ['singularity', 'jacket'],
-          sassDir: '<%= yeoman.app %>/_scss',
-          cssDir: '.tmp/css',
-          imagesDir: '<%= yeoman.app %>/images',
-          javascriptsDir: '<%= yeoman.app %>/js',
-          relativeAssets: false,
-          httpImagesPath: '/images',
-          httpGeneratedImagesPath: '/images/generated',
-          outputStyle: 'expanded',
-          raw: 'extensions_dir = "<%= yeoman.app %>/_bower_components"\n',
-          importPath: ['<%= yeoman.app %>/_bower_components/foundation/scss', '<%= yeoman.app %>/_bower_components/fontawesome/scss', '<%= yeoman.app %>/_bower_components/chartist/dist/scss'],
+          generatedImagesDir: '<%= yeoman.dist %>/img/generated'
         }
       },
       server: {
@@ -263,11 +251,19 @@ module.exports = function (grunt) {
             // Usemin moves CSS and javascript inside of Usemin blocks.
             // Copy moves asset files and directories.
             'img/**/*',
+            'css/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
-            //'_bower_components/jquery/jquery.min.js',
+            '_bower_components/jquery/jquery.min.js',
+            '_bower_components/foundation/js/foundation.js',
+            '_bower_components/foundation/js/foundation/foundation.topbar.js',
+            '_bower_components/foundation/js/foundation/foundation.interchange.js',
+            '_bower_components/foundation/js/foundation/foundation.equalizer.js',
+            '_bower_components/svg4everybody/svg4everybody.ie8.min.js',
+            '_bower_components/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+            '_bower_components/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js'
             //'favicon.ico',
             //'apple-touch*.png'
           ],
