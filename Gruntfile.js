08c2b8be3e2717c7ee61b5ac8cf063cd6a226b6b
Evan Jenkins <evan@activelamp.com>
2015-06-07 17:20:07 -0700

    removing css from file rev

--- Gruntfile.js
+++ Gruntfile.js
@@ -288,8 +288,8 @@ module.exports = function (grunt) {
         files: [{
           src: [
             '<%= yeoman.dist %>/js/**/*.js',
-            '<%= yeoman.dist %>/css/**/*.css',
-            '<%= yeoman.dist %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
+            //'<%= yeoman.dist %>/css/**/*.css',
+            //'<%= yeoman.dist %>/img/**/*.{gif,jpg,jpeg,png,svg,webp}',
             '<%= yeoman.dist %>/fonts/**/*.{eot*,otf,svg,ttf,woff}'
           ]
         }]
